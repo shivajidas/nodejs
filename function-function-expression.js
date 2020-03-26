62156fec926993d03a0bb3d47a6be91b66398d78
Shivaji <shivajidas30@gmail.com>
2020-03-27 01:56:53 +0530

    committing basic practice files

---
+++ function-function-expression.js
@@ -0,0 +1,11 @@
+//normal js function
+function sayHi(){
+console.log('hi');
+}
+sayHi();
+
+//fuinction expression using anonymous fuinction
+var sayBye=function(){
+console.log('bye');
+}
+sayBye();
